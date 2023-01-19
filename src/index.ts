a270d2ad00de608d1e14ceddd60634ea034563ee
EUNSEO814 <eunseo814@gmail.com>
2023-01-19 20:11:43 +0900

    ✨ typescript- blockchain

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// const hello = () => "hi";
+
+// class Block {
+//   constructor(private data: string) {}
+//   static hello() {
+//     return "hello";
+//   }
+// }
+
+import { init, exit } from "./myPackage";
+
+// init({
+//   debug: false,
+//   url: "ss",
+// });
